f542d7c6fe51f868a764008888abe885f3cb8818
jot4hpe <jot4hpe.com>
2024-05-06 13:23:11 -0300

    primeiro commit

---
+++ tabuada.py
@@ -0,0 +1,34 @@
+n1 = int(input('digite um numero: '))
+x1 = n1 * 1
+x2 = n1 * 2
+x3 = n1 * 3
+x4 = n1 * 4
+x5 = n1 * 5 
+x6 = n1 * 6
+x7 = n1 * 7
+x8 = n1 * 8
+x9 = n1 * 9
+x10 = n1 * 10
+print(f'{n1} x 1 = {x1}')
+print('|----------|')
+print(f'{n1} x 2 = {x2}')
+print('|----------|')
+print(f'{n1} x 3 = {x3}')
+print('|----------|')
+print(f'{n1} x 4 = {x4}')
+print('|----------|')
+print(f'{n1} x 5 = {x5}')
+print('|----------|')
+print(f'{n1} x 6 = {x6}')
+print('|----------|')
+print(f'{n1} x 7 = {x7}')
+print('|----------|')
+print(f'{n1} x 8 = {x8}')
+print('|----------|')
+print(f'{n1} x 9 = {x9}')
+print('|----------|')
+print(f'{n1} x 10 = {x10}')
+print('|----------|')
+
+
+
